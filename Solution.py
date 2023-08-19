a2547447bdcc2875d64f247bcb8617152153330a
Welberth Azevedo Barbosa <welberthab@gmail.com>
2023-08-19 18:25:24 -0300

    Solution

---
+++ Solution.py
@@ -0,0 +1,13 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root):
+        if root:
+            invert = self.invertTree
+            root.left, root.right = invert(root.right), invert(root.left)
+            return root
